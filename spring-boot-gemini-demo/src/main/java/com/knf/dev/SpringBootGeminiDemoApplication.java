09c63d5f86fe1c21b7c6ddbded99f5493fa1aa88
mary3773 <maryriya15449@gmail.com>
2024-12-19 14:12:56 +0530

    Add files via upload

---
+++ spring-boot-gemini-demo/src/main/java/com/knf/dev/SpringBootGeminiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.knf.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGeminiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGeminiDemoApplication.class, args);
+	}
+
+}
